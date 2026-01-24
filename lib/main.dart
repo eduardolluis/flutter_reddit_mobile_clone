420d5c79694dc085f1ba72b1890076791cb9ce62
eduardolluis <1132907@est.intec.edu.do>
2026-01-24 17:12:26 -0400

    flutter project setup

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:reddit_clone/theme/pallete.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reddit ',
+      theme: Pallete.darkModeAppTheme,
+      home: const SizedBox(),
+    );
+  }
+}
