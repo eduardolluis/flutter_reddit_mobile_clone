dd521d756e867eb9d016821cc6dd6b504b6a8b8c
eduardolluis <1132907@est.intec.edu.do>
2026-01-25 11:03:35 -0400

    error hadling technique

--- lib/core/common/sign_in_button.dart
+++ lib/core/common/sign_in_button.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/constants/constants.dart';
-import 'package:reddit_clone/features/controller/auth_controller.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 
 class SignInButton extends ConsumerWidget {
   const SignInButton({super.key});
 
-  void signInWithGoogle(WidgetRef ref) {
-    ref.read(authControllerProvider).signInWithGoogle();
+  void signInWithGoogle(BuildContext context, WidgetRef ref) {
+    ref.read(authControllerProvider).signInWithGoogle(context);
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ElevatedButton.icon(
-      onPressed: () => signInWithGoogle(ref),
+      onPressed: () => signInWithGoogle(context, ref),
       icon: Image.asset(Constants.googlePath, width: 35),
       label: Text('Sign in with Google', style: TextStyle(color: Colors.white)),
     );
--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,11 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:reddit_clone/core/constants/constants.dart';
 import 'package:reddit_clone/core/constants/firebase_constants.dart';
+import 'package:reddit_clone/core/failure.dart';
 import 'package:reddit_clone/core/providers/firebase_providers.dart';
+import 'package:reddit_clone/core/type_defs.dart';
 import 'package:reddit_clone/models/user_model.dart';
+
+final userProvider = StateProvider<UserModel?>((ref) => null);
 
 final authRepositoryProvider = Provider(
   (ref) => AuthRepository(
@@ -31,11 +37,13 @@ class AuthRepository {
   CollectionReference get _users =>
       _firestore.collection(FirebaseConstants.usersCollection);
 
-  Future<void> signInWithGoogle() async {
+  FutureEither<UserModel> signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
 
-      if (googleUser == null) return;
+      if (googleUser == null) {
+        return left(Failure(message: 'Google sign-in cancelled'));
+      }
 
       final googleAuth = await googleUser.authentication;
 
@@ -44,9 +52,7 @@ class AuthRepository {
         idToken: googleAuth.idToken,
       );
 
-      UserCredential userCredential = await _auth.signInWithCredential(
-        credential,
-      );
+      final userCredential = await _auth.signInWithCredential(credential);
 
       UserModel userModel;
 
@@ -60,10 +66,25 @@ class AuthRepository {
           karma: 0,
           awards: [],
         );
+
         await _users.doc(userModel.uid).set(userModel.toMap());
+      } else {
+        final doc = await _users.doc(userCredential.user!.uid).get();
+
+        userModel = UserModel.fromMap(doc.data() as Map<String, dynamic>);
       }
-    } catch (e) {
-      print(e);
+
+      return right(userModel);
+    } on FirebaseAuthException catch (e) {
+      return left(Failure(message: e.toString()));
     }
+  }
+
+  Stream<UserModel> getUserData(String uid) {
+    return _users.doc(uid).snapshots().map(
+          (event) => UserModel.fromMap(
+            event.data() as Map<String, dynamic>,
+          ),
+        );
   }
 }
