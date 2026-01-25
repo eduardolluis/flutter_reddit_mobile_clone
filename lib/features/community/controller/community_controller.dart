70807aa5d72a8f3d3ffca0cd585e3e029400323b
eduardolluis <1132907@est.intec.edu.do>
2026-01-25 16:44:44 -0400

    creatubg communities and storing to firebase

---
+++ lib/features/community/controller/community_controller.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:reddit_clone/core/constants/constants.dart';
+import 'package:reddit_clone/core/utils.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:reddit_clone/features/community/screens/repository/community_repository.dart';
+import 'package:reddit_clone/models/community_model.dart';
+import 'package:routemaster/routemaster.dart';
+
+final communityControllerProvider =
+    StateNotifierProvider<CommunityController, bool>((ref) {
+      return CommunityController(
+        communityRepository: ref.watch(CommunityRepositoryProvider),
+        ref: ref,
+      );
+    });
+
+class CommunityController extends StateNotifier<bool> {
+  final CommunityRepository _communityRepository;
+  final Ref _ref;
+
+  CommunityController({
+    required CommunityRepository communityRepository,
+    required Ref ref,
+  }) : _ref = ref,
+       _communityRepository = communityRepository,
+       super(false);
+
+  void createCommunity(String name, BuildContext context) async {
+    state = true;
+    final uid = _ref.read(userProvider)?.uid ?? '';
+    Community community = Community(
+      id: name,
+      name: name,
+      banner: Constants.bannerDefault,
+      avatar: Constants.avatarDefault,
+      members: [],
+      mods: [],
+    );
+
+    final res = await _communityRepository.createCommunity(community);
+    state = false;
+    res.fold((l) => showSnackBar(context, l.message), (r) {
+      showSnackBar(context, 'Community created successfully!');
+      showSnackBar(context, 'Community created successfully!');
+      Routemaster.of(context).pop();
+    });
+  }
+}
--- lib/features/community/screens/create_community_screen.dart
+++ lib/features/community/screens/create_community_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/loader.dart';
+import 'package:reddit_clone/features/community/controller/community_controller.dart';
 
 class CreateCommunityScreen extends ConsumerStatefulWidget {
   const CreateCommunityScreen({super.key});
@@ -18,43 +20,55 @@ class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
     communityNameController.dispose();
   }
 
+  void createCommunity() {
+    ref
+        .read(communityControllerProvider.notifier)
+        .createCommunity(communityNameController.text.trim(), context);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(communityControllerProvider);
     return Scaffold(
       appBar: AppBar(title: const Text("Create a Community")),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            Align(alignment: Alignment.topLeft, child: Text("Community name")),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'r/Community_name',
-                filled: true,
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.all(18),
-              ),
-              maxLength: 21,
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {},
+      body: isLoading
+          ? const Loader()
+          : Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text("Community name"),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'r/Community_name',
+                      filled: true,
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(18),
+                    ),
+                    maxLength: 21,
+                  ),
+                  SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () => createCommunity(),
 
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              child: Text(
-                "Create Community",
-                style: TextStyle(color: Colors.white, fontSize: 17),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: Text(
+                      "Create Community",
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
