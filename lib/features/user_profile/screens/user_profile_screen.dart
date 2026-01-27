0535a85fb61cb4363e78470d8014170c4bc89cb4
eduardolluis <1132907@est.intec.edu.do>
2026-01-27 17:24:27 -0400

    user profile UI

---
+++ lib/features/user_profile/screens/user_profile_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/error_text.dart';
+import 'package:reddit_clone/core/common/loader.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
+
+class UserProfileScreen extends ConsumerStatefulWidget {
+  final String uid;
+
+  const UserProfileScreen({super.key, required this.uid});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends ConsumerState<UserProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ref
+          .watch(getUserDataProvider(widget.uid))
+          .when(
+            data: (user) => NestedScrollView(
+              headerSliverBuilder: (context, innerBoxIsScrolled) {
+                return [
+                  SliverAppBar(
+                    expandedHeight: 250,
+                    floating: true,
+                    snap: true,
+                    flexibleSpace: Stack(
+                      children: [
+                        Positioned.fill(
+                          child: Image.network(user.banner, fit: BoxFit.cover),
+                        ),
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Container(
+                            alignment: Alignment.bottomLeft,
+                            padding: const EdgeInsets.all(
+                              20,
+                            ).copyWith(bottom: 70),
+                            child: CircleAvatar(
+                              backgroundImage: NetworkImage(user.profilePic),
+                              radius: 45,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          padding: const EdgeInsets.all(20),
+                          child: OutlinedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 25,
+                              ),
+                            ),
+                            child: const Text("Edit Profile"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.all(16),
+                    sliver: SliverList(
+                      delegate: SliverChildListDelegate([
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'u/${user.name}',
+                              style: const TextStyle(
+                                fontSize: 19,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Text("${user.karma} karma"),
+                        ),
+                        const SizedBox(height: 10),
+                        const Divider(thickness: 2),
+                      ]),
+                    ),
+                  ),
+                ];
+              },
+              body: const Text("Displaying posts"),
+            ),
+            error: (error, stackTrace) => ErrorText(error: error.toString()),
+            loading: () => const Loader(),
+          ),
+    );
+  }
+}
--- lib/router.dart
+++ lib/router.dart
@@ -9,6 +9,7 @@ import 'package:reddit_clone/features/community/screens/edit_community_screen.da
 import 'package:reddit_clone/features/community/screens/mod_tools_screen.dart';
 import 'package:reddit_clone/features/home/screens/home_screen.dart';
 import 'package:reddit_clone/features/screens/login_screen.dart';
+import 'package:reddit_clone/features/user_profile/screens/user_profile_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
 final loggedOutRoute = RouteMap(
@@ -31,6 +32,10 @@ final loggedInRoute = RouteMap(
     ),
     '/add-mods/:name': (routeData) => MaterialPage(
       child: AddModsScreen(name: routeData.pathParameters['name']!),
+    ),
+
+    '/u/:uid': (routeData) => MaterialPage(
+      child: UserProfileScreen(uid: routeData.pathParameters['uid']!),
     ),
   },
 );
